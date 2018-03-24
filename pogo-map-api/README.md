f196bb93bddc86f18bb8af5fb70f58b61651d489
tchampion <tchampion@xebia.fr>
2018-03-24 20:45:16 +0100

    Add cors to enable query from everywhere

--- pogo-map-api/README.md
+++ pogo-map-api/README.md
@@ -7,3 +7,7 @@ docker pull postgres
 
 docker run --name postgres-3 -d -p 5432:5432 postgres:latest
 ```
+
+psql -U postgres -d postgres -a -f resources/03-gyms.sql -h localhost
+psql -U postgres -d postgres -a -f resources/03-gyms.sql -h localhost
+psql -U postgres -d postgres -a -f resources/03-gyms.sql -h localhost
--- pogo-map-api/src/main.ts
+++ pogo-map-api/src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { ApplicationModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {ApplicationModule} from './app.module';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+
 
 async function bootstrap() {
-	const app = await NestFactory.create(ApplicationModule);
-	await app.listen(3000);
+    const app = await NestFactory.create(ApplicationModule);
+
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    await app.listen(3000);
 }
+
 bootstrap();
