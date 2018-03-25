6c34c8151b37d261da48a92d0423cd5e7f950bba
tchampion <tchampion@xebia.fr>
2018-03-25 18:47:02 +0200

    Restore tileLayer attribution

--- pogo-map-ui/src/App.vue
+++ pogo-map-ui/src/App.vue
@@ -132,7 +132,10 @@
       this.tileLayers.push({
         url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}',
         options: {
-          attribution: 'aa',
+          attribution: `Map tiles by
+            <a href="http://stamen.com">Stamen Design</a>,
+            <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy;
+            <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`,
           subdomains: 'abcd',
           minZoom: 0,
           maxZoom: 20,
