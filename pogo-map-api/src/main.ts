807be06b34743d905e5398451d3718147f6c5a41
tchampion <tchampion@xebia.fr>
2018-03-26 22:37:40 +0200

    Listen on PORT env variable or 3000 if undefined

--- pogo-map-api/src/main.ts
+++ pogo-map-api/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     app.use(bodyParser.json());
     app.use(cors());
 
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
