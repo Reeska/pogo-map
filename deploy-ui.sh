e8d60bd9088840e05b7cf292f3eb64ced9dbcd6a
tchampion <tchampion@xebia.fr>
2018-03-27 10:40:34 +0200

    Test light way to deploy UI (with existing directory = not ignored)

--- deploy-ui.sh
+++ deploy-ui.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SOURCE=pogo-map-ui
-DIST="$SOURCE/dist"
+DIST="$SOURCE/deploy"
 
 cp "$SOURCE/package-heroku.json" "$DIST/package.json"
 git subtree push --prefix "$DIST" https://git.heroku.com/pokemongogymmap-web.git master
