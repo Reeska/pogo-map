5e9a611043dcdef01d20d1c0ea1e047bd3fd5368
tchampion <tchampion@xebia.fr>
2018-04-08 14:06:48 +0200

    Build, commit & push before deployment

--- deploy-ui.sh
+++ deploy-ui.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+cd pogo-map-ui
+echo "> Build"
+npm run build
+git add .
+
+echo "> Commit & push"
+git commit -m "Deployment"
+git push
+cd -
+
+echo "> Deployment"
 git subtree push --prefix pogo-map-ui https://git.heroku.com/pokemongogymmap-web.git master
